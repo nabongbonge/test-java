17d963a54f301e6c92bef4c7d16d21cb89b0bfc5
nabongbonge <jaeseugi.1927@gmail.com>
2023-02-14 22:30:17 +0900

    Study 도메인 속성 추가
    StudyStatus 속성과 limit 속성

--- src/main/java/com/example/testjava/Study.java
+++ src/main/java/com/example/testjava/Study.java
@@ -1,4 +1,24 @@
 package com.example.testjava;
 
 public class Study {
+
+  private StudyStatus status;
+
+  private int limit;
+
+  public Study(int limit) {
+    if (limit < 0) {
+      throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+    }
+    this.limit = limit;
+  }
+
+  public StudyStatus getStatus() {
+    return status;
+  }
+
+  public int getLimit() {
+    return limit;
+  }
+
 }
