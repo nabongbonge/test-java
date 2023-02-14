1375bd1f08700ec15a0e29f1d0f69a570caa3cf1
nabongbonge <jaeseugi.1927@gmail.com>
2023-02-14 21:52:49 +0900

    JUnit5 테스트 이름 표시하기
    * @DisplayNameGeneration
    * @DisplayName

--- src/test/java/com/example/testjava/StudyTest.java
+++ src/test/java/com/example/testjava/StudyTest.java
@@ -4,17 +4,19 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
   @Test
-  void create() {
+  @DisplayName("스터디 만들기")
+  void create_new_study() {
     Study study = new Study();
     assertNotNull(study);
     System.out.println("create");
   }
 
   @Test
-  void create1() {
+  void create_new_study1() {
     System.out.println("create1");
   }
 
