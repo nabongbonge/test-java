be255d5bf80b88b40432df33030580532baab139
nabongbonge <jaeseugi.1927@gmail.com>
2023-02-14 21:41:33 +0900

    JUnit5 기본 어노테이션
    * @Test
    * @BeforeAll / @AfterAll
    * @BeforeEach / @AfterEach
    * @Disabled

---
+++ src/test/java/com/example/testjava/StudyTest.java
@@ -0,0 +1,51 @@
+package com.example.testjava;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class StudyTest {
+
+  @Test
+  void create() {
+    Study study = new Study();
+    assertNotNull(study);
+    System.out.println("create");
+  }
+
+  @Test
+  void create1() {
+    System.out.println("create1");
+  }
+
+  /**
+   * 모든 테스트 호출되기 전 딱 한번만 호출
+   * 반드시 접근제어자는 private 사용X
+   * 반드시 static 선언
+   * 반드시 리턴 타입은 void 사용
+   */
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("before all");
+  }
+
+  @Disabled // 테스트 제외
+  @AfterAll
+  static void afterAll() {
+    System.out.println("after all");
+  }
+
+  /**
+   * 각 테스트 호출되기 전에 호출
+   */
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("before each");
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("after each");
+  }
+
+}
