0a02922c9c1e5724478f7764ae425d24f8e9dddc
nabongbonge <jaeseugi.1927@gmail.com>
2023-02-15 20:03:37 +0900

    스터디 상태 추가
    OPENED 추가

--- src/main/java/com/example/testjava/StudyStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.testjava;
-
-public enum StudyStatus {
-  DRAFT, STARTED, ENDED
-}
---
+++ src/main/java/com/example/testjava/study/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.example.testjava.study;
+
+public enum StudyStatus {
+  DRAFT, STARTED, OPENED, ENDED
+}
