2ec34d5c00e9a9b8194149636a835a10794be168
nabongbonge <jaeseugi.1927@gmail.com>
2023-02-15 20:03:00 +0900

    스터디 서비스 추가

---
+++ src/main/java/com/example/testjava/study/StudyService.java
@@ -0,0 +1,26 @@
+package com.example.testjava.study;
+
+import com.example.testjava.domain.Member;
+import com.example.testjava.domain.Study;
+import com.example.testjava.member.MemberService;
+
+import java.util.Optional;
+
+public class StudyService {
+
+  private final MemberService memberService;
+  private final StudyRepository repository;
+
+  public StudyService(MemberService memberService, StudyRepository repository) {
+    assert memberService != null; // java.lang.AssertionError at com.example.testjava.study.StudyService.<init>(StudyService.java:15)
+    assert repository != null;
+    this.memberService = memberService;
+    this.repository = repository;
+  }
+
+  public Study createNewStudy(Long memberId, Study study) {
+    Optional<Member> member = memberService.findById(memberId);
+    study.setOwner(member.orElseThrow(() -> new IllegalArgumentException("Member dosen`t exist for id : '" + memberId + "'")));
+    return repository.save(study);
+  }
+}
